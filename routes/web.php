29069514887c1ba4cf1f3bce0dd7dc6fd809c1fc
Mujahed <mu.mujahed.maaz@gmail.com>
2025-09-13 14:08:00 +0300

    Create the controller and model for Category

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,11 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 //Route::view('/', 'home')->name('home');
-Route::get('/', function() {
-    $categories = DB::table('categories')->get();
-    return view('home', ['categories' => $categories]);
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::view('contact', 'contact')->name('contact');
 // the route name is used inside blade like this
 // {{ route('rout_name') }} so if we changed the URL
