a8e9b8f5975a1fe5127d0477ac1ef6aa13e5622d
Mujahed <mu.mujahed.maaz@gmail.com>
2025-09-13 13:50:47 +0300

    retrieve data from DB inside routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home')->name('home');
+//Route::view('/', 'home')->name('home');
+Route::get('/', function() {
+    $categories = DB::table('categories')->get();
+    return view('home', ['categories' => $categories]);
+});
 Route::view('contact', 'contact')->name('contact');
 // the route name is used inside blade like this
 // {{ route('rout_name') }} so if we changed the URL
